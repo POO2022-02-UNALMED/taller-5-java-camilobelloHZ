34d669da844573c0e379af5a50540f17003162c6
camilobelloHZ <cbelloh@unal.edu.co>
2022-09-25 21:41:33 -0500

    prueba 11

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -7,7 +7,8 @@ public class Zona {
 	private  static ArrayList<Zoologico> zoo= new ArrayList<Zoologico>();
 	private   ArrayList<Animal> animales = new ArrayList<Animal>();
 	
-
+	//contructores 
+	
 	public Zona(String nombre,Zoologico zoo) {
 		this.nombre = nombre;
 		Zona.zoo.add(zoo);
@@ -17,6 +18,7 @@ public class Zona {
 		
 	}
 	
+	//metodos de la clase 
 	public void agregarAnimales(Animal animales) {
 		this.animales.add(animales); 
 		
@@ -26,25 +28,30 @@ public class Zona {
 		return getAnimales().size();
 	}
 
+	//metodos get
 	public String getNombre() {
 		return nombre;
 	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+	
+	public ArrayList<Animal> getAnimales() {
+		return animales;
 	}
-
+	
 	public static Zoologico getZoo() {
 		return Zona.zoo.get(0);
 	}
 
+
+	//metodos set
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	
 	public static void setZoo(Zoologico zoo) {
 		Zona.zoo.add(zoo);
 	}
 
-	public ArrayList<Animal> getAnimales() {
-		return animales;
-	}
 
 	public void setAnimales(Animal animales) {
 		this.animales.add(animales);
