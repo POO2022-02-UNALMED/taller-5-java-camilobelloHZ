6d698f3c341cf65ceccb40b48c1c63b8f1bcbd26
camilobelloHZ <cbelloh@unal.edu.co>
2022-09-25 20:32:42 -0500

    prueba 10

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -7,7 +7,7 @@ import java.util.*;
 		private ArrayList<Zona> zonas = new ArrayList<Zona>(); 
 		
 		
-		
+		// contructores
 		public Zoologico(String nombre, String ubicacion) {
 			this.nombre = nombre;
 			this.ubicacion = ubicacion;
@@ -17,44 +17,48 @@ import java.util.*;
 			
 		}
 		
-		public void agregarZonas(Zona zonas) {
-			this.zonas.add(zonas);
-			
-		}
-		
-		public int cantidadTotalAnimales() {
-			int suma=0;
-			for (int i=0;i<zonas.size();i++) {
-				suma += zonas.get(i).cantidadAnimales();
-			}
-			return suma; 
-			
-			
-		}
-
-		public String getNombre() {
-			return nombre;
-		}
-
+		//metodos set 
 		public void setNombre(String nombre) {
 			this.nombre = nombre;
+		}
+		
+		public void setUbicacion(String ubicacion) {
+			this.ubicacion = ubicacion;
+		}
+		
+		public void setZonas(Zona zonas) {
+			this.zonas.add(zonas); 
+		}
+
+		// metodos set 
+		public String getNombre() {
+			return nombre;
 		}
 
 		public String getUbicacion() {
 			return ubicacion;
 		}
 
-		public void setUbicacion(String ubicacion) {
-			this.ubicacion = ubicacion;
-		}
-
-		public ArrayList<Zona> getZona() {//getZonas
-			return zonas;
-		}
-
-		public void setZonas(Zona zonas) {
-			this.zonas.add(zonas); 
-		}
 		
 
-}
+		public ArrayList<Zona> getZona() {
+			return zonas;
+		}
+		
+		// metodos de la clase 
+		public void agregarZonas(Zona zonas) {
+					this.zonas.add(zonas);
+					
+				}
+				
+		public int cantidadTotalAnimales() {
+			int total=0;
+				for (int e=0;e<zonas.size();e++) {
+						total += zonas.get(e).cantidadAnimales();
+				}
+			return total; 
+
+		
+		}
+		
+	}
